ac665284ab4aa8983524fce6fefe35695fa3d898
flareware95 <flareware95@gmail.com>
2025-02-22 15:25:20 -0500

    a

---
+++ src/main/java/frc/team1126/commands/subsystems/arm/MoveExtHome.java
@@ -0,0 +1,37 @@
+package frc.team1126.commands.subsystems.arm;
+
+import com.revrobotics.spark.ClosedLoopSlot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.team1126.RobotContainer;
+import frc.team1126.subsystems.ArmSubsystem;
+import frc.team1126.subsystems.ExtensionSubsystem;
+
+public class MoveExtHome extends Command {
+
+   private ExtensionSubsystem extension;
+   private ArmSubsystem arm;
+   private double targetExtension;
+   private ClosedLoopSlot slot;
+
+   public MoveExtHome(ExtensionSubsystem extension, ArmSubsystem arm, double pos, ClosedLoopSlot slot) {
+       addRequirements(RobotContainer.m_extension);
+       this.extension = extension;
+       this.arm = arm;
+       targetExtension = pos;
+       this.slot = slot;
+   }
+
+   @Override
+   public void execute() {
+        extension.extReachGoal(targetExtension,slot);
+       
+    
+   }
+
+   @Override
+   public boolean isFinished() {
+       return false;
+   }
+
+}
